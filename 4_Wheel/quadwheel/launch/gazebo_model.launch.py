10363ad3b6b647ff521ae430e10a1d88a326d438
BastianWeiss <ba379142@ucf.edu>
2025-04-03 02:25:58 -0400

    launch file fixed

--- 4_Wheel/quadwheel/launch/gazebo_model.launch.py
+++ 4_Wheel/quadwheel/launch/gazebo_model.launch.py
@@ -1,100 +1,122 @@
+# Gazebo Launch File for Quadwheel Base
+# Created By Bastian Weiss
+#---------------------------------------------
+# Desired Output: Creates quadwheel model in rviz and gazebo.
+#---------------------------------------------
+
+# Imports
+# For os path commands
 import os
+
+# Importing Model and world and launch files
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ros_gz_bridge.actions import RosGzBridge
-from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import Command, LaunchConfiguration
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch_ros.actions import Node
-import xacro
-import launch
+
+# Import Ros Launch
 import launch_ros
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
-#Launching
+# Core structure
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+
+# Gazebo
+from ros_gz_bridge.actions import RosGzBridge
+from ros_gz_sim.actions import GzServer
+
+# Xacro
+import xacro
+
+#----------------------------------------------------------------------------
+# Launch
 def generate_launch_description():
-    
-    # this name has to match the robot name in the Xacro file
-    robotXacroName='4_wheel_omni'
-    
-    # this is the name of our package, at the same time this is the name of the 
-    # folder that will be used to define the paths
-    namePackage = 'quadwheel'
-    
-    # this is a relative path to the xacro file defining the model
-    modelFileRelativePath = 'model/main.xacro'
-    # this is a relative path to the Gazebo world file
-    worldFileRelativePath = 'model/empty_world.world'
-    
-    # this is the absolute path to the model
-    pathModelFile = os.path.join(get_package_share_directory(namePackage),modelFileRelativePath)
 
-    # this is the absolute path to the world model
-    pathWorldFile = os.path.join(get_package_share_directory(namePackage),worldFileRelativePath)
-    # get the robot description from the xacro model file
-    robotDescription = xacro.process_file(pathModelFile).toxml()
+	# Names
+	package_name = 'quadwheel'
+	robot_name = '4_wheel_omni'
+	world_name = 'lab_world.world'
+	urdf_name = 'main.xacro'
+	urdf_folder_name = 'model'
 
-    # Find Package Possibly
-    ros_gz_sim = get_package_share_directory('ros_gz_sim')    
+	#-------------------------------------------------------------------------------
+	# Variables
+	world = LaunchConfiguration('world') 
+	
+	# Paths
+	path_to_urdf = os.path.join(get_package_share_directory(package_name),urdf_folder_name,urdf_name)
+	path_to_world = os.path.join(get_package_share_directory(package_name),urdf_folder_name,world_name)
 
-    # this is the launch file from the ros_gz package
-    ros_gzPackageLaunch=PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('ros_gz_sim'),
-                                                                       'launch','gz_sim.launch.py'))
-    # this is the launch description   
-    gazeboLaunch=IncludeLaunchDescription(ros_gzPackageLaunch,launch_arguments={'world': pathWorldFile}.items())
-  
-    # here, we create a ros_gz Node 
-    spawnModelNode = Node(package='ros_gz_sim', executable='create',
-                          arguments=['-topic','robot_description','-name', robotXacroName],output='screen')
-    
-    # Robot State Publisher Node
-    nodeRobotStatePublisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robotDescription,
-        'use_sim_time': True}] 
-    )
+	# Robot Description 
+	robot_description = xacro.process_file(path_to_urdf).toxml()
+	
+	#Find ros_gz_sim package
+	pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+	gz_launch_path = PythonLaunchDescriptionSource(os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'))
 
-    ##RVIZ2 Component
-    #Joint State Publisher Node
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{'robot_description': robotDescription,'use_sim_time': True}],
-        
-    )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        
-    )
-    #RVIZ2 Launch
-    rviz = os.path.join(get_package_share_directory('quadwheel'), 'model.rviz')
-    nodeRVIZ2 = Node(
-    	package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen'
-    
-    )
-    # here we create an empty launch description object
-    launchDescriptionObject = LaunchDescription()
-     
-    # we add gazeboLaunch 
-    launchDescriptionObject.add_action(gazeboLaunch)
-    
-    # we add the nodes
-    launchDescriptionObject.add_action(spawnModelNode)
-    launchDescriptionObject.add_action(nodeRobotStatePublisher)
-    launchDescriptionObject.add_action(joint_state_publisher_node)
-    launchDescriptionObject.add_action(joint_state_publisher_gui_node)
-    launchDescriptionObject.add_action(nodeRVIZ2)
-    
+	#Launch Configs
+	declare_world_cmd = DeclareLaunchArgument(
+    		name='world',
+    		default_value=path_to_world,
+    		description='Full path to the world model file to load'
+	)
 
-    return launchDescriptionObject
+	# Launch Gazebo
+	gz_server = IncludeLaunchDescription(gz_launch_path,
+		launch_arguments={'gz_args':path_to_world}.items(),
+	)
 
+	# Spawn Gazebo Model
+	spawn_model_node = Node(package='ros_gz_sim', executable='create',
+		arguments=['-topic','robot_description','-name', robot_name],output='screen')
 
+	# Publishers
+	# Robot State Publisher
+	robot_state_publisher_node = launch_ros.actions.Node(
+   		package='robot_state_publisher',
+        	executable='robot_state_publisher',
+		output='screen',
+        	parameters=[{'robot_description': robot_description,
+			     'use_sim_time': True,
+		           }],     
+	)
+		
+	# Joint State Publisher
+	joint_state_publisher_node = launch_ros.actions.Node(
+       		package='joint_state_publisher',
+        	executable='joint_state_publisher',
+        	name='joint_state_publisher',
+        	parameters=[{'robot_description': robot_description,
+			     'use_sim_time': True,
+		           }],
+      
+   	)
+
+	joint_state_publisher_gui_node = launch_ros.actions.Node(
+        	package='joint_state_publisher_gui',
+        	executable='joint_state_publisher_gui',
+        	name='joint_state_publisher_gui',
+	)
+	
+	# RVIZ
+	rviz_node = Node(
+    		package='rviz2',
+        	executable='rviz2',
+        	name='rviz2',
+        	output='screen'
+    	)
+       
+	# here we create an empty launch description object
+	ld = LaunchDescription()
+
+	# Add Launch Nodes
+	ld.add_action(declare_world_cmd)
+	ld.add_action(gz_server)
+	ld.add_action(spawn_model_node)
+	ld.add_action(robot_state_publisher_node)
+	ld.add_action(joint_state_publisher_node)
+	ld.add_action(joint_state_publisher_gui_node)
+	ld.add_action(rviz_node)
+
+	return ld
